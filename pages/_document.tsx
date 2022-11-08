6c6744118b307d7a257f16f16b7913c328648dd1
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-08 18:21:11 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=optional"
                         rel="stylesheet"
                     />
+                    <script src="https://embed.zenn.studio/js/listen-embed-event.js" />
                 </Head>
                 <body>
                     <Main />
