e86955f402191493e5f3b854d0c109baa8408812
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-08 09:25:33 +0000

    ok

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,39 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
+import Image from "next/image";
+import Link from "next/link";
 
 const nav_items = [
-    {
-        name: 'Introduce',
-        href: '/introduce',
-    }
-]
+  {
+    name: "Introduce",
+    href: "/introduce",
+  },
+];
 
 export default function Header() {
-    return (
-        <header className="header">
-            <div className="header__brand">
-                <Image className='header__brand-logo' src="/logo.webp" alt="logo" width={64} height={64} />
-                <h1 className='header__brand-title'>
-                    <Link href="/">fdc notes</Link>
-                </h1>
-            </div>
-            <nav className="header__nav">
-                <ul className="header__nav-list">
-                    {nav_items.map((item) => (
-                        <li className="header__nav-items" key={item.name}>
-                            <Link className="header__nav-item" href={item.href}>{item.name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="header__brand">
+        <Image
+          className="header__brand-logo"
+          src="/logo.webp"
+          alt="logo"
+          width={64}
+          height={64}
+        />
+        <h1 className="header__brand-title">
+          <Link href="/">fdc notes</Link>
+        </h1>
+      </div>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          {nav_items.map((item) => (
+            <li className="header__nav-items" key={item.name}>
+              <Link className="header__nav-item" href={item.href}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
